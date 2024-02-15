60da0eb69de7cf32dd87a81435a3be025d025ddd
jackiezzhen <jackiezhen31@gmail.com>
2024-02-15 09:07:09 -0500

    AHHHHHHHHH

--- vue/src/views/wishView.vue
+++ vue/src/views/wishView.vue
@@ -1,21 +1,33 @@
 <template>
-<div>
-    <button @click="random"></button>
-  </div>
-</template>
-
-<script setup>
-  function random(){
-    Math.random();
+  <div>
+      <button v-on:click="console.log("ahhh")">ggtyj</button>
+    </div>
+  </template>
+  
+  <script setup>
+    const rates = [1, 16, 23, 50]
+    function random(rates) {
+      let sum = 0;
+      rates.forEach(function(chance) {
+          sum+=chance;
+      });
+      let rand = Math.random();
+      let chance = 0;
+      for(let i=0; i<rates.length; i++) {
+          chance+=rates[i]/sum;
+          if(rand<chance) {
+              return i;
+          }
+          return -1;
+      }}
+  </script>
+  
+  <style scoped>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
   }
-</script>
-
-<style scoped>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+  </style>
